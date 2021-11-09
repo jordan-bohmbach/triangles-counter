e13c561f7f539be234fe178cfee2165ea2e6fd9a
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-11-09 10:13:25 -0600

    removed reference to deleted file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-
-
-
-if (process.env.NODE_ENV !== "production") {
-
-
-
-}
 
 function Root() {
   return (
